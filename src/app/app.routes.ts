988ed1f1e12c474c3ba45c1905ee256cd5140c10
Andre Funk <andre.funk@icloud.com>
2026-04-01 09:37:42 +0200

    feat: imprit page added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
 import { Content } from './layout/content/content';
+import { Imprint } from './layout/imprint/imprint';
 
 export const routes: Routes = [
   {
     path: '',
     component: Content,
+  },
+  {
+    path: 'imprint',
+    component: Imprint,
   },
   {
     path: '**',
