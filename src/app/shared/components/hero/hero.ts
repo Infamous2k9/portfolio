445ec4ddc4e4fbf3aa5abfc9c73debbe60fd0edb
Andre Funk <Andre.funk.94@web.de>
2026-04-03 06:05:01 +0200

    feat: Hero and Header done just need little fixes and style

--- src/app/shared/components/hero/hero.ts
+++ src/app/shared/components/hero/hero.ts
@@ -1,9 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { Header } from '../../../layout/header/header';
+import { TranslatePipe } from '@ngx-translate/core';
+import { UpperCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-hero',
-  imports: [],
+  imports: [Header, TranslatePipe, UpperCasePipe],
   templateUrl: './hero.html',
   styleUrl: './hero.scss',
 })
-export class Hero {}
+export class Hero {
+  isHovering = false;
+  innerWidth = 0;
+  nameTag = {
+    text: 'André :)',
+    hover: 'André :D',
+  };
+  headlineText = {
+    fullstack: ['f', 'u', 'l', 'l', 's', 't', 'a', 'c', 'k'],
+    developer: ['d', 'e', 'v', 'e', 'l', 'o', 'p', 'e', 'r'],
+  };
+  ngOnInit() {
+    this.innerWidth = window.innerWidth;
+  }
+  @HostListener('window:resize')
+  onResize() {
+    this.innerWidth = window.innerWidth;
+  }
+
+  isUppercase(item: string) {
+    return item === item.toUpperCase();
+  }
+}
