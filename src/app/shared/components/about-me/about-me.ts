2c791b8fea433d4d2874fe62285d4e9206bb9086
Andre Funk <Andre.funk.94@web.de>
2026-04-10 14:20:02 +0200

    feat: aboutme section finish only mobile version

--- src/app/shared/components/about-me/about-me.ts
+++ src/app/shared/components/about-me/about-me.ts
@@ -1,9 +1,41 @@
 import { Component } from '@angular/core';
+import { Snipet } from '../snipet/snipet';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-about-me',
-  imports: [],
+  imports: [Snipet, TranslatePipe],
   templateUrl: './about-me.html',
   styleUrl: './about-me.scss',
 })
-export class AboutMe {}
+export class AboutMe {
+  snipets = [
+    {
+      icon: '/icons/Location.png',
+      text: 'aboutme.snipets.text.location',
+      backgroundImages: {
+        mobile: '/images/02_Ripped-Paper/Option-1/Mobile/About-me-1.png',
+        desktop: '/images/02_Ripped-Paper/Option-1/1-Yellow.png',
+        alt: 'aboutme.snipets.background.opt1.mobile.yellow.name',
+      },
+    },
+    {
+      icon: '/icons/Relocation.png',
+      text: 'aboutme.snipets.text.relocation',
+      backgroundImages: {
+        mobile: '/images/02_Ripped-Paper/Option-1/Mobile/About-me-2.png',
+        desktop: '/images/02_Ripped-Paper/Option-1/2-bkue.png',
+        alt: 'aboutme.snipets.background.opt1.mobile.blue.name',
+      },
+    },
+    {
+      icon: '/icons/Remote.png',
+      text: 'aboutme.snipets.text.remote',
+      backgroundImages: {
+        mobile: '/images/02_Ripped-Paper/Option-1/Mobile/About-me-3.png',
+        desktop: '/images/02_Ripped-Paper/Option-1/3-Orange.png',
+        alt: 'aboutme.snipets.background.opt1.mobile.orange.name',
+      },
+    },
+  ];
+}
---
+++ src/app/shared/components/snipet/snipet.ts
@@ -0,0 +1,20 @@
+import { Component, input } from '@angular/core';
+import { TranslatePipe, TranslateDirective } from '@ngx-translate/core';
+
+interface Background {
+  mobile: string;
+  desktop: string;
+  alt: string;
+}
+
+@Component({
+  selector: 'app-snipet',
+  imports: [TranslatePipe, TranslateDirective],
+  templateUrl: './snipet.html',
+  styleUrl: './snipet.scss',
+})
+export class Snipet {
+  text = input.required<string>();
+  icon = input.required();
+  background = input.required<Background>();
+}
